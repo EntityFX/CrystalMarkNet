25db06fb87bad3fecb79f2c94ff1b197b91d6149
EntityFX <artem.solopiy@gmail.com>
2021-04-02 12:51:01 +0300

    Use shared cancellation atomic

--- src/EntityFx.CrystalMark/EntityFx.CrystalMark/CrystalBenchmarkBase.cpp
+++ src/EntityFx.CrystalMark/EntityFx.CrystalMark/CrystalBenchmarkBase.cpp
@@ -10,20 +10,19 @@ int CrystalBenchmarkBase::bench(int threads) {
 int CrystalBenchmarkBase::bench_all(int threads)
 {
 	std::future<int>* futures = new std::future<int>[threads];
-	std::atomic_bool* cancellation_tokens = new std::atomic_bool[threads];
+	std::atomic_bool cancellation_token;
+
 
 	std::vector<int> readyFutures;
 
 	for (int i = 0; i < threads; ++i) {
-		futures[i] = std::async(std::launch::async, &CrystalBenchmarkBase::bench_implementation, this, std::ref(cancellation_tokens[i]));
+		futures[i] = std::async(std::launch::async, &CrystalBenchmarkBase::bench_implementation, this, std::ref(cancellation_token));
 	}
 
 	std::chrono::milliseconds span(10000);
 
 	while (futures[0].wait_for(span) == std::future_status::timeout) {
-		for (int i = 0; i < threads; ++i) {
-			cancellation_tokens[i] = true;
-		}
+		cancellation_token = true;
 	}
 
 	int validFutures = 0;
@@ -47,7 +46,5 @@ int CrystalBenchmarkBase::bench_all(int threads)
 		result += n;
 
 	delete [] futures;
-	delete [] cancellation_tokens;
-
 	return result;
 }
